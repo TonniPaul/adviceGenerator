431168d2fe4d1380ae42e6fcd424de93f1529b8f
TonniPaul <ariyoadeoyepaul@gmail.com>
2022-10-12 00:25:34 +0100

    Added frontend mentor tag

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,10 @@ function App() {
           <img src={dice} alt="" />
         </div>
       </div>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://tonnipaul.live">TonniPaul</a>.
+      </div>
     </div>
   )
 }
